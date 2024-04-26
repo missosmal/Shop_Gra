67f6ff4855926214437c78d5effb055f2e27c956
lmvs <valomovskikh@list.ru>
2024-04-26 14:40:16 +0500

    Разработанный контроллер предметов.Graf

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop_Graf37.Data.Interfaces;
+
+namespace Shop_Graf37.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategorys IAllCategorys;
+
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategorys = IAllCategorys;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop_Graf37.Data.Models;
+using Shop_Graf37.Data.Mocks;
+using Shop_Graf37.Data.Interfaces;
 
 namespace Shop_Graf37
 {
@@ -16,6 +19,8 @@ namespace Shop_Graf37
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategorys, MockCaregorys>();
+
             services.AddMvc();
         }
 
