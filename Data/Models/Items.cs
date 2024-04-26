ae43a57fe7d3c98e7a466b1872bc03ba994e14d9
lmvs <valomovskikh@list.ru>
2024-04-26 13:49:00 +0500

    Разработанные модели данных.GRaf

---
+++ Data/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_Graf37.Data.Models
+{
+    public class Items
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categorys Category{ get; set; }
+    }
+}
